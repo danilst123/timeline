6efe34bce7e1c750baafeb58d3f0fe5fcf3ed620
DSStepanov <DSStepanov@lanit.ru>
2020-06-10 19:54:47 +0500

    первая версия слайдера

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(function () {
+  function setSizes() {
+    setTimeout(function () {
+      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+      let vh = window.innerHeight * 0.01;
+      // Then we set the value in the --vh custom property to the root of the document
+      document.documentElement.style.setProperty("--vh", vh + "px");
+      //document.body.style.setProperty("--vh", vh + "px");
+    }, 50);
+  }
+
+  setSizes();
+
+  $(window).resize(function () {
+    setSizes();
+  });
+});
